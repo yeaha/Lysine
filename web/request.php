d387a7824b82510b3d8257a4bd8e551f827a3707
yangyi <yangyi.cn.gz@gmail.com>
2010-05-30 14:47:25 +0800

    http request 对象草稿

---
+++ web/request.php
@@ -0,0 +1,106 @@
+<?php
+class Ly_Request {
+    protected function _getFrom($source, $key = null, $default = false) {
+        if (is_null($key)) return $source;
+        return isset($source[$key]) ? $source[$key] : $default;
+    }
+
+    public function get($key = null, $default = false) {
+        return $this->_getFrom($_GET, $key, $default);
+    }
+
+    public function post($key = null, $default = false) {
+        return $this->_getFrom($_POST, $key, $default);
+    }
+
+    public function request($key = null, $default = false) {
+        return $this->_getFrom($_REQUEST, $key, $default);
+    }
+
+    public function cookie($key = null, $default = false) {
+        return $this->_getFrom($_COOKIE, $key, $default);
+    }
+
+    public function env($key = null, $default = false) {
+        return $this->_getFrom($_ENV, strtoupper($key), $default);
+    }
+
+    public function server($key = null, $default = false) {
+        return $this->_getFrom($_SERVER, strtoupper($key), $default);
+    }
+
+    public function file() {
+    }
+
+    public function header($key) {
+        $skey = 'http_'. str_replace('-', '_', $key);
+        $sval = $this->server($skey);
+        if ($sval) return $sval;
+
+        return false;
+    }
+
+    public function requestMethod() {
+        $method = $this->header('x-http-method-override');
+        return strtolower( $method ? $method : $this->server('request_method') );
+    }
+
+    public function requestUri() {
+        $uri = $this->server('http_x_rewrite_url');
+        if ($uri) return $uri;
+        return $this->server('request_uri');
+    }
+
+    public function isGET() {
+        return $this->requestMethod() === 'get';
+    }
+
+    public function isPOST() {
+        return $this->requestMethod() === 'post';
+    }
+
+    public function isPUT() {
+        return $this->requestMethod() === 'put';
+    }
+
+    public function isDELETE() {
+        return $this->requestMethod() === 'delete';
+    }
+
+    protected function _getAccept($key) {
+        $result = array();
+        if (!$accept = $this->server($key)) return $result;
+
+        foreach (explode(',', $accept) as $accept) {
+            $pos = strpos($accept, ';');
+            if ($pos !== false) $accept = substr($accept, 0, $pos);
+            $result[] = strtolower(trim($accept));
+        }
+
+        return $result;
+    }
+
+    public function acceptTypes() {
+        return $this->_getAccept('http_accept');
+    }
+
+    public function acceptLang() {
+        return $this->_getAccept('http_accept_language');
+    }
+
+    public function acceptCharset() {
+        return $this->_getAccept('http_accept_charset');
+    }
+
+    public function acceptEncoding() {
+        return $this->_getAccept('http_accept_encoding');
+    }
+
+    public function referer() {
+        return $this->server('http_referer');
+    }
+
+    public function ip() {
+        return $this->server('remote_addr', '127.0.0.1');
+    }
+}
