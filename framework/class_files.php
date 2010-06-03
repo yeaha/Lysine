43a078c1c1eef5998836587c67de9ba0fbe1841c
yangyi <yangyi.cn.gz@gmail.com>
2010-06-04 00:59:32 +0800

    更新类映射数据

--- framework/class_files.php
+++ framework/class_files.php
@@ -1,9 +1,11 @@
 <?php
 return array(
     'Lysine' => 'core.php',
-    'Ly_Controller' => 'web/controller.php',
     'Ly_Response' => 'web/response.php',
     'Ly_Request' => 'web/request.php',
+    'Ly_Helper_Curl' => 'helper/curl.php',
+    'Ly_Cache_Memcached' => 'cache/memcached.php',
+    'Ly_Exception' => 'base/exception.php',
     'Ly_Application' => 'base/application.php',
     'Ly_Db_Adapter_Interface' => 'base/interface.php'
 );
