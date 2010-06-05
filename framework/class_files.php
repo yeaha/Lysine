88c4a05f4e02d5aac8f8c2f0abfd7be1c60cbb2e
yangyi <yangyi.cn.gz@gmail.com>
2010-06-05 13:53:51 +0800

    更新类映射数组

--- framework/class_files.php
+++ framework/class_files.php
@@ -3,9 +3,11 @@ return array(
     'Lysine' => 'core.php',
     'Ly_Response' => 'web/response.php',
     'Ly_Request' => 'web/request.php',
+    'Ly_Request_Exception' => 'web/request.php',
     'Ly_Helper_Curl' => 'helper/curl.php',
     'Ly_Cache_Memcached' => 'cache/memcached.php',
     'Ly_Exception' => 'base/exception.php',
     'Ly_Application' => 'base/application.php',
-    'Ly_Db_Adapter_Interface' => 'base/interface.php'
+    'Ly_Db_Adapter_Interface' => 'base/interface.php',
+    'Ly_Cache_Interface' => 'base/interface.php'
 );
