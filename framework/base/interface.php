70b403c3c879ba2700eff026971aa2dc7f5b8185
yangyi <yangyi.cn.gz@gmail.com>
2010-06-13 16:21:41 +0800

    实现Memcached缓存类

--- framework/base/interface.php
+++ framework/base/interface.php
@@ -1,6 +1,39 @@
 <?php
+/**
+ * 缓存类接口
+ * 采用统一的接口便于封装cache frontend类
+ *
+ * @author yangyi <yangyi@surveypie.com>
+ */
 interface Ly_Cache_Interface {
-    public function set();
-    public function get();
-    public function delete();
+    /**
+     * 保存缓存数据
+     *
+     * @param string $key
+     * @param mixed $val
+     * @param integer $life_time
+     * @access public
+     * @return boolean
+     */
+    public function set($key, $val, $life_time = null);
+
+    /**
+     * 获得缓存数据
+     * 如果没有数据，回调callback参数
+     *
+     * @param string $key
+     * @param callback $callback
+     * @access public
+     * @return mixed
+     */
+    public function get($key, $callback = null);
+
+    /**
+     * delete
+     *
+     * @param string $key
+     * @access public
+     * @return boolean
+     */
+    public function delete($key);
 }
--- framework/cache/memcached.php
+++ framework/cache/memcached.php
@@ -6,55 +6,75 @@
  * @author Yang Yi <yangyi.cn.gz@gmail.com>
  */
 class Ly_Cache_Memcached implements Ly_Cache_Interface {
-    /**
-     * 服务器连接
-     *
-     * @var mixed
-     * @access protected
-     */
     protected $conn;
+    protected $default_life_time;
 
-    public function __construct(array $policy = null) {
+    /**
+     * 构造函数
+     * array(
+     *     'default_life_time' => 0,
+     *     'servers' => array(
+     *         array('192.168.1.100', 11211, 60),
+     *         array('192.168.1.101', 11211, 40),
+     *     ),
+     *     'options' => array(
+     *         Memcached::OPT_PREFIX_KEY => 'app id',
+     *         Memcached::OPT_SERIALIZER => Memcached::OPT_SERIALIZER_JSON,
+     *         Memcached::OPT_HASH => Memcache::HASH_MD5,
+     *         Memcached::OPT_DISTRIBUTION => Memcache::DISTRIBUTION_CONSISTENT,
+     *     )
+     * )
+     * @param array $options
+     * @access public
+     * @return void
+     */
+    public function __construct(array $config = null) {
+        if (!extension_loaded('memcached'))
+            throw new RuntimeException('Memcached extension must be loaded before use.');
+
+        $conn = new Memcached();
+
+        if (!$config) $config = array();
+        $options = isset($config['options']) ? $config['options'] : array();
+        $servers = isset($config['servers']) ? $config['servers'] : array(array('127.0.0.1', 11211));
+        $this->default_life_time = isset($config['default_life_time']) ? $config['default_life_time'] : 0;
+
+        foreach ($options as $opt => $val) $conn->setOptions($opt, $val);
+        $conn->addServers($servers);
+
+        $this->conn = $conn;
     }
 
-    /**
-     * 除了get() set() delete()之外
-     * 其他的调用全部直接转发给后端连接对象
-     *
-     * @param string $fn
-     * @param array $args
-     * @access public
-     * @return mixed
-     */
     public function __call($fn, $args) {
-        if (!$this->conn) return false;
-
-        if (method_exists($this->conn, $fn))
-            return call_user_func_array(array($this->conn, $fn), $args);
-
-        throw new BadMethodCallException('Ly_Cache_Memcached: call bad method '. $fn);
+        return call_user_func_array(array($this->conn, $fn), $args);
     }
 
     /**
      * 保存缓存数据
      *
-     * @param mixed $key
+     * @param string $key
      * @param mixed $val
      * @param integer $life_time
      * @access public
      * @return boolean
      */
     public function set($key, $val, $life_time = null) {
+        if (!$life_time) $life_time = $this->default_life_time;
+        $expire_time = $life_time ? (now() + $life_time) : 0;
+        return $this->conn->set($key, $val, $expire_time);
     }
 
     /**
      * 获得缓存数据
      *
-     * @param mixed $key
+     * @param string $key
+     * @param callback $callback
      * @access public
      * @return mixed
      */
-    public function get($key) {
+    public function get($key, $callback = null) {
+        $args = func_get_args();
+        return call_user_func_array(array($this->conn, 'get'), $args);
     }
 
     /**
@@ -65,5 +85,7 @@ class Ly_Cache_Memcached implements Ly_Cache_Interface {
      * @return boolean
      */
     public function delete($key) {
+        $args = func_get_args();
+        return call_user_func_array(array($this->conn, 'delete'), $args);
     }
 }
