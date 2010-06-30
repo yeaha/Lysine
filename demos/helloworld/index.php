4d3962664e12668a223a1ba1721f034dfe9f8145
yangyi <yangyi.cn.gz@gmail.com>
2010-06-30 17:20:15 +0800

    更新hello world demo

--- demos/helloworld/index.php
+++ demos/helloworld/index.php
@@ -2,11 +2,15 @@
 require_once __DIR__ .'/../../framework/core.php';
 use Lysine as ly;
 
-$urls = array(
-    '#/(.*)#' => '\Controller\index',
+$config = array(
+    'app' => array(
+        'router' => array(
+            '#/(.*)#' => '\Controller\index',
+        ),
+    ),
 );
 
 $app = ly\app()->includePath(__DIR__);
 
-$resp = $app->run($urls);
+$resp = $app->setConfig($config)->run();
 echo $resp;
