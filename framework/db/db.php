43cdb9ad7a75c098afd9c190e35c0be10deec540
yangyi <yangyi.cn.gz@gmail.com>
2010-07-26 10:44:40 +0800

    增加Db::parseConfig()方法，集中处理数据库配置解析

--- framework/db/db.php
+++ framework/db/db.php
@@ -20,6 +20,21 @@ class Db {
         return self::$default_path = $path;
     }
 
+    static public function parseConfig(array $cfg) {
+        if (!isset($cfg['dsn']))
+            throw new \InvalidArgumentException('Invalid database config');
+
+        $dsn = $cfg['dsn'];
+
+        $user = isset($cfg['user']) ? $cfg['user'] : null;
+        $pass = isset($cfg['pass']) ? $cfg['pass'] : null;
+        $options = (isset($cfg['options']) AND is_array($cfg['options']))
+                 ? $cfg['options']
+                 : array();
+
+        return array($dsn, $user, $pass, $options);
+    }
+
     static public function connect($path = null) {
         if ($path === null) {
             $path = self::$default_path;
@@ -28,17 +43,9 @@ class Db {
         }
         $cfg = cfg($path);
 
-        if (!is_array($cfg) OR !isset($cfg['dsn']))
-            throw new \InvalidArgumentException('database config['. implode(', ', $path) .'] not found!');
-
-        $dsn = $cfg['dsn'];
+        list($dsn, $user, $pass, $options) = self::parseConfig($cfg);
         if (isset(self::$adapter[$dsn])) return self::$adapter[$dsn];
 
-        $user = isset($cfg['user']) ? $cfg['user'] : null;
-        $pass = isset($cfg['pass']) ? $cfg['pass'] : null;
-        $options = (isset($cfg['options']) AND is_array($cfg['options']))
-                 ? $cfg['options']
-                 : array();
         $adapter = self::factory($dsn, $user, $pass, $options);
         self::$adapter[$dsn] = $adapter;
         return $adapter;
