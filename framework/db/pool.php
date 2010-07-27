0e691b76256c2a7bd2acf2503976f4162f2e0b90
yangyi <yangyi.cn.gz@gmail.com>
2010-07-27 10:28:20 +0800

    更换更合适的变量名

--- framework/db/pool.php
+++ framework/db/pool.php
@@ -18,7 +18,7 @@ class Pool {
 
     protected $adapter = array();
 
-    protected $current = '__default__';
+    protected $current_node = '__default__';
 
     /**
      * 构造函数
@@ -58,47 +58,47 @@ class Pool {
     /**
      * 切换数据库连接
      *
-     * @param string $name
+     * @param string $node_name
      * @access public
      * @return self
      */
-    public function useDb($name) {
-        $this->current = $name;
+    public function useNode($node_name) {
+        $this->current_node = $node_name;
         return $this;
     }
 
     /**
      * 获得数据库连接
      *
-     * @param mixed $name
+     * @param mixed $node_name
      * @access public
      * @return Lysine\Db\Adapter
      */
-    public function getAdapter($name = null) {
-        if ($name === null) $name = $this->current;
+    public function getAdapter($node_name = null) {
+        if ($node_name === null) $node_name = $this->current_node;
 
-        if (!isset($this->adapter[$name])) {
-            if (!isset($this->servers[$name]))
-                throw new \InvalidArgumentException('Adapter ['. $name .'] not found');
+        if (!isset($this->adapter[$node_name])) {
+            if (!isset($this->servers[$node_name]))
+                throw new \InvalidArgumentException('Adapter ['. $node_name .'] not found');
 
-            list($dsn, $user, $pass, $options) = $this->servers[$name];
+            list($dsn, $user, $pass, $options) = $this->servers[$node_name];
 
-            $this->adapter[$name] = Db::factory($dsn, $user, $pass, $options);
+            $this->adapter[$node_name] = Db::factory($dsn, $user, $pass, $options);
         }
 
-        return $this->adapter[$name];
+        return $this->adapter[$node_name];
     }
 
     /**
      * 添加一个server到列表里
      *
-     * @param mixed $string
+     * @param string $node_name
      * @param array $config
      * @access public
      * @return self
      */
-    public function addServer($name, array $config) {
-        $this->servers[$name] = Db::parseConfig($config);
+    public function addServer($node_name, array $config) {
+        $this->servers[$node_name] = Db::parseConfig($config);
         return $this;
     }
 
@@ -110,7 +110,7 @@ class Pool {
      * @return self
      */
     public function addServers(array $servers) {
-        foreach ($servers as $name => $config) $this->addServer($name, $config);
+        foreach ($servers as $node_name => $config) $this->addServer($node_name, $config);
         return $this;
     }
 
@@ -143,8 +143,8 @@ class Pool {
             throw new \InvalidArgumentException('Group ['. $group .'] not found');
 
         $fn = $this->dispatcher[$group];
-        $name = call_user_func($fn, $token);
-        return $this->getAdapter($name);
+        $node_name = call_user_func($fn, $token);
+        return $this->getAdapter($node_name);
     }
 
     /**
