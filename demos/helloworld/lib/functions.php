6e2862b8051683da6cf3c48fb362b0402f0f97eb
yangyi <yangyi.cn.gz@gmail.com>
2010-10-12 13:50:01 +0800

    更新namespace

--- demos/helloworld/lib/functions.php
+++ demos/helloworld/lib/functions.php
@@ -1,16 +1,16 @@
 <?php
 function render_view($view_name, array $vars = null) {
     static $render;
-    if (!$render) $render = new Lysine\View(cfg('app', 'view'));
+    if (!$render) $render = new Lysine\MVC\View(cfg('app', 'view'));
 
     return $render->reset()->fetch($view_name, $vars);
 }
 
 function __on_exception($exception) {
-    $code = $exception instanceof \Lysine\Request_Exception
+    $code = $exception instanceof \Lysine\HttpError
           ? $exception->getCode()
           : 500;
-    header(\Lysine\Response::httpStatus($code));
+    header(\Lysine\MVC\Response::httpStatus($code));
 
     echo render_view('_error/500', array('exception' => $exception));
     die($code);
