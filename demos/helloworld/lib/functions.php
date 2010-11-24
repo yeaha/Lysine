cee1e3a2ee9cb5ae4fc56cddb3093682c9cd41f3
yangyi <yangyi.cn.gz@gmail.com>
2010-11-24 16:42:50 +0800

    更新demo

--- demos/helloworld/lib/functions.php
+++ demos/helloworld/lib/functions.php
@@ -1,12 +1,8 @@
 <?php
 function __on_exception($exception) {
-    $code = $exception instanceof \Lysine\HttpError
-          ? $exception->getCode()
-          : 500;
-    header(\Lysine\MVC\Response::httpStatus($code));
-
+    $code = \Lysine\__on_exception($exception);
     require_once ROOT_DIR .'/public/_error/500.php';
-    die($code);
+    die(1);
 }
 
 function __on_error($errno, $errstr, $errfile, $errline, $errcontext) {
