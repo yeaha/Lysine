2770a1666ed69c9ca012c9396c391b2d92b1d905
yangyi <yangyi.cn.gz@gmail.com>
2010-11-24 21:03:33 +0800

    调整demo

--- demos/helloworld/config/boot.php
+++ demos/helloworld/config/boot.php
@@ -6,8 +6,17 @@ require_once __DIR__ .'/../../../framework/core.php';
 Lysine\Utils\Profiler::instance()->start('__MAIN__');
 Lysine\Config::import(require_once ROOT_DIR .'/config/_config.php');
 
-require_once ROOT_DIR .'/lib/functions.php';
-set_exception_handler('__on_exception');
-set_error_handler('__on_error');
-
 app()->includePath(ROOT_DIR .'/app');
+
+set_exception_handler(function($exception) {
+    $code = \Lysine\__on_exception($exception);
+    require_once ROOT_DIR .'/public/_error/500.php';
+    die(1);
+});
+
+set_error_handler(function($errno, $errstr, $errfile, $errline, $errcontext) {
+    throw new Error($errstr, $errno, null, array(
+        'file' => $errfile,
+        'line' => $errline,
+    ));
+});
