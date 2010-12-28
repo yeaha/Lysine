d413699b89b701c4ae5f096e39271ba85294d0d4
yangyi <yangyi.cn.gz@gmail.com>
2010-12-28 14:34:03 +0800

    helloworld demo增加日志配置

--- demos/helloworld/config/_config.php
+++ demos/helloworld/config/_config.php
@@ -5,4 +5,10 @@ return array(
             'view_dir' => ROOT_DIR .'/app/view',
         ),
     ),
+    'storages' => array(
+        'lysine_log' => array(
+            'class' => 'Lysine\Storage\File',
+            'filename' => ROOT_DIR .'/logs/lysine_%Y-%m-%d.log',
+        ),
+    ),
 );
