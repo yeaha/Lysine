d3ba937c4df0ac61e7bbd8e20b4707dbffae5831
yangyi <yangyi.cn.gz@gmail.com>
2010-12-30 15:22:37 +0800

    更新demo

--- demos/helloworld/config/boot.php
+++ demos/helloworld/config/boot.php
@@ -15,20 +15,11 @@ set_exception_handler(function($exception) {
         echo $exception;
     } else {
         list($code, $header) = \Lysine\__on_exception($exception);
+        ob_start();
         require ROOT_DIR .'/public/_error/500.php';
+        echo ob_get_clean();
     }
     die(1);
-});
-
-set_error_handler(function($errno, $errstr, $errfile, $errline, $errcontext) {
-    $logger = Lysine\logger();
-    if (in_array($errno, array(E_ERROR, E_CORE_ERROR, E_COMPILE_ERROR, E_USER_ERROR))) {
-        $logger->error($errstr);
-    } elseif (in_array($errno, array(E_WARNING, E_CORE_WARNING, E_COMPILE_WARNING, E_USER_WARNING))) {
-        $logger->warning($errstr);
-    } else {
-        $logger->debug($errstr);
-    }
 });
 
 // 日志配置
